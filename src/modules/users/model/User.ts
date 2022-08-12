32e87d6128e8186d96c207b8eb5c28f7ca06c8d6
mayconrralves@yahoo.com.br <mayconrralves@yahoo.com.br>
2022-08-11 21:13:49 -0300

    all tests are correct

--- src/modules/users/model/User.ts
+++ src/modules/users/model/User.ts
@@ -2,6 +2,22 @@ import { v4 as uuidV4 } from "uuid";
 
 class User {
   // Complete aqui
-}
+  id: string;
+  name: string;
+  email: string;
+  admin: boolean;
+  created_at: Date;
+  updated_at: Date;
 
+  constructor() {
+    if (!this.id) this.id = uuidV4();
+    this.admin = false;
+    this.created_at = new Date();
+    this.updated_at = new Date();
+  }
+  public changeAdmin(): void {
+    this.admin = !this.admin;
+    this.updated_at = new Date();
+  }
+}
 export { User };
